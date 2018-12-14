b502cc159a5585735a0dc5cd6cd574547ce44d1d
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-12-13 22:27:47 -0300

    feat: add UI

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLable: UILabel!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var phraseSearchButton: UIButton!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    @IBOutlet weak var latLonSearchButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: Search Actions
 
-
+    @IBAction func searchByPhrase(_ sender: Any) {
+        
+    }
+    
+    @IBAction func searchByLatLon(_ sender: Any) {
+        
+    }
 }
 
