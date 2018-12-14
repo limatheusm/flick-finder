deff8f0382ae75ef085890c5d821a2ccebe8dc5b
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-12-13 23:41:17 -0300

    feat: Handle keyboard to avoid textfield

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    var keyboardOnScreen = false
+    
     // MARK: Outlets
     
     @IBOutlet weak var photoImageView: UIImageView!
@@ -23,7 +27,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        phraseTextField.delegate = self
+        latitudeTextField.delegate = self
+        longitudeTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
     
     // MARK: Search Actions
@@ -34,6 +44,86 @@ class ViewController: UIViewController {
     
     @IBAction func searchByLatLon(_ sender: Any) {
         
+    }
+}
+
+// MARK: - ViewController: UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Show/Hide Keyboard
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if !keyboardOnScreen {
+            view.frame.origin.y -= keyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if keyboardOnScreen {
+            view.frame.origin.y += keyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardDidShow(_ notification: Notification) {
+        keyboardOnScreen = true
+    }
+    
+    @objc func keyboardDidHide(_ notification: Notification) {
+        keyboardOnScreen = false
+    }
+    
+    func keyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = (notification as NSNotification).userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func resignIfFirstResponder(_ textField: UITextField) {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func userDidTapView(_ sender: AnyObject) {
+        // Only one or none text field will respond
+        resignIfFirstResponder(phraseTextField)
+        resignIfFirstResponder(latitudeTextField)
+        resignIfFirstResponder(longitudeTextField)
+    }
+    
+    // MARK: TextField Validation
+    
+    func isTextFieldValid(_ textField: UITextField, forRange: (Double, Double)) -> Bool {
+        if let value = Double(textField.text!), !textField.text!.isEmpty {
+            return isValueInRange(value, min: forRange.0, max: forRange.1)
+        } else {
+            return false
+        }
+    }
+    
+    func isValueInRange(_ value: Double, min: Double, max: Double) -> Bool {
+        return !(value < min || value > max)
+    }
+}
+
+// MARK: - ViewController (Notifications)
+
+extension ViewController {
+    
+    func subscribeToNotification(_ notification: NSNotification.Name, selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    func unsubscribeFromAllNotifications() {
+        NotificationCenter.default.removeObserver(self)
     }
 }
 
